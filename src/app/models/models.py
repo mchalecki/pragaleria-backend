6e53dbc755f20e8ac81ccb223d5487ca358df3a0
Marcin Chalecki <alcaster0@gmail.com>
2018-09-02 22:45:13 +0200

    Set database url to general docker name in cofnig file. Fix formatting.

--- src/app/models/models.py
+++ src/app/models/models.py
@@ -2,25 +2,25 @@ from app.application import db
 
 
 class Terms(db.Model):
-	__tablename__ = '2c191c_terms'
-	term_id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(200))
-	slug = db.Column(db.String(200))
-	term_group = db.Column(db.Integer)
-	term_order = db.Column(db.Integer)
+    __tablename__ = '2c191c_terms'
+    term_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200))
+    slug = db.Column(db.String(200))
+    term_group = db.Column(db.Integer)
+    term_order = db.Column(db.Integer)
 
-	def __repr__(self):
-		return 'id: {}, name: {}'.format(self.term_id, self.name)
+    def __repr__(self):
+        return 'id: {}, name: {}'.format(self.term_id, self.name)
 
 
 class TermTaxonomies(db.Model):
-	__tablename__ = '2c191c_term_taxonomy'
-	term_taxonomy_id = db.Column(db.Integer, primary_key=True)
-	term_id = db.Column(db.Integer)
-	taxonomy = db.Column(db.String(32))
-	description = db.Column(db.Text())
+    __tablename__ = '2c191c_term_taxonomy'
+    term_taxonomy_id = db.Column(db.Integer, primary_key=True)
+    term_id = db.Column(db.Integer)
+    taxonomy = db.Column(db.String(32))
+    description = db.Column(db.Text())
 
-	def __repr__(self):
-		return 'term_id: {}, taxonomy: {}, description: {}'.format(
-			self.term_id, self.taxonomy, self.description
-		)
+    def __repr__(self):
+        return 'term_id: {}, taxonomy: {}, description: {}'.format(
+            self.term_id, self.taxonomy, self.description
+        )
