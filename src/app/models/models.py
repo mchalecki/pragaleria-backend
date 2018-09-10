c88c63cdbd7a1307027529760a1df1b61a06f7e6
Adam Przywarty <przywarty.it@gmail.com>
2018-09-10 21:32:48 +0200

    Add auctions api.

--- src/app/models/models.py
+++ src/app/models/models.py
@@ -24,3 +24,29 @@ class TermTaxonomies(db.Model):
         return 'term_id: {}, taxonomy: {}, description: {}'.format(
             self.term_id, self.taxonomy, self.description
         )
+
+class Posts(db.Model):
+    __tablename__ = '2c191c_posts'
+    id = db.Column(db.Integer, primary_key=True)
+    post_author = db.Column(db.Integer)
+    post_date = db.Column(db.DateTime)
+    post_date_gmt = db.Column(db.DateTime)
+    post_content = db.Column(db.Text())
+    post_title = db.Column(db.Text())
+    post_excerpt = db.Column(db.Text())
+    post_status = db.Column(db.String(20))
+    comment_status = db.Column(db.String(20))
+    ping_status = db.Column(db.String(20))
+    post_password = db.Column(db.String(255))
+    post_name = db.Column(db.String(200))
+    to_ping = db.Column(db.Text())
+    pinged = db.Column(db.Text())
+    post_modified = db.Column(db.DateTime)
+    post_modified_gmt = db.Column(db.DateTime)
+    post_content_filtered = db.Column(db.Text())
+    post_parent = db.Column(db.Integer)
+    guid = db.Column(db.String(255))
+    menu_order = db.Column(db.Integer)
+    post_type = db.Column(db.String(20))
+    post_mime_type = db.Column(db.String(100))
+    comment_count = db.Column(db.Integer)
--- src/app/resources/api.py
+++ src/app/resources/api.py
@@ -1,10 +1,52 @@
-from flask import Blueprint
+import json
+
+from flask import Blueprint, make_response
 from flask_restful import Resource, Api, abort
+from sqlalchemy.sql import func
 
 from app.models import models
 
 api_bl = Blueprint('api', __name__, url_prefix='/api')
 api = Api(api_bl)
+
+
+@api.representation('application/json')
+def output_json(data, code, headers=None):
+    resp = make_response(
+        json.dumps(
+            data,
+            ensure_ascii=False
+        ).encode('utf8'),
+    code)
+    resp.headers.extend(headers or {})
+    return resp
+
+
+class Auctions(Resource):
+    def get(self):
+        try:
+            auctions = models.Posts.query.filter(
+                models.Posts.guid.like(
+                    '%aukcje-wystawy%'
+                )
+            ).filter(
+                models.Posts.post_name.like(
+                    '%aukcja%'
+                )
+            ).order_by(models.Posts.post_date.desc())
+        except Exception as e:
+            abort(404, message='Error querying Posts. {}'.format(e))
+        result = []
+        for auction in auctions:
+            if auction.post_title and auction.post_excerpt:
+                result.append({
+                    'id': auction.id,
+                    'title': auction.post_title,
+                    'description': auction.post_excerpt,
+                    'guid': auction.guid,
+                    'date': str(auction.post_date)
+                })
+        return result
 
 
 class Terms(Resource):
@@ -38,3 +80,4 @@ def get_term_description(term_id):
 
 
 api.add_resource(Terms, '/terms')
+api.add_resource(Auctions, '/auctions')
