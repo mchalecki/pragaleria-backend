c938bfcf7037a8fb2414f5b1b04ee11c5f90d620
Marcin Chalecki <alcaster0@gmail.com>
2018-11-13 09:12:35 +0100

    Adjust tests. Fix regex to cases when it was matching numbers without 'x'

--- src/app/api_utils/regex_utils.py
+++ src/app/api_utils/regex_utils.py
@@ -1,16 +1,11 @@
 import re
 from typing import List
 
-dimension_regex = re.compile(r"(\d+(,\d)?(\s?x\s?)?)+")
+dimension_regex = re.compile(r"\d+(,\d)?(\s?x\s?)\d+(,\d)?((\s?x\s?)\d+(,\d)?)?")
 
 
 def get_dimensions_from_description(description: str) -> List[float]:
-    # TODO: bad cases:
-    # serigrafia, papier, e.a. 1/3, 67 x 49 cm (w świetle passe-partout), 85 x 66 cm (oprawa), sygn. p.d.
-    # akwaforta, akwatinta, papier, 51/100, 79 x 79 cm (arkusz), 65 x 65 cm (odcisk płyty);
-    # serigrafia, papier, e.a. 1/3, 67 x 49 cm (w świetle passe-partout), 85 x 66 cm (oprawa), sygn. p.d.
-    # drewno, żywica epoksydowa, technika własna artysty, ed. 42-100, 7 x 11,5 x 8,5 cm, sygnowana na bocznej ścianie
-    # mezzotinta, sucha igła, 10/30, 70 x 100 cm, sygn. u dołu
+    # TODO: add tests for hard cases
     dimensions = dimension_regex.search(description)
     if dimensions:
         dimensions = dimensions.group()  # TODO handle exceptions, more tests
--- src/app/tests/api_endpoints/test_catalog.py
+++ src/app/tests/api_endpoints/test_catalog.py
@@ -1,44 +1,48 @@
 import json
+from typing import List
 
 
 def get_endpoint_result(client, endpoint):
-	res = client.get(endpoint)
-	return json.loads(res.data.decode('utf-8'))
+    res = client.get(endpoint)
+    return json.loads(res.data.decode('utf-8'))
 
 
 class TestCatalog:
-	def test_count(self, client):
-		res = client.get('/api/catalog/12179')
-		data = json.loads(res.data.decode('utf-8'))
-		assert len(data) == 20
+    def test_count(self, client):
+        res = client.get('/api/catalog/12179')
+        data = json.loads(res.data.decode('utf-8'))
+        assert len(data) == 20
 
-	def test_last_entry(self, client):
-		res = client.get('/api/catalog/12179')
-		data = json.loads(res.data.decode('utf-8'))
-		assert data[-1]['title'] == 'Komedia romantyczna'
+    def test_last_entry(self, client):
+        res = client.get('/api/catalog/12179')
+        data = json.loads(res.data.decode('utf-8'))
+        assert data[-1]['title'] == 'Komedia romantyczna'
 
-	def test_404_if_catalog_does_not_exist(self, client):
-		res = client.get('/api/catalog/997')
-		data = json.loads(res.data.decode('utf-8'))
-		assert 'Catalog for this auction does not exist. id: 997' in data['message']
+    def test_404_if_catalog_does_not_exist(self, client):
+        res = client.get('/api/catalog/997')
+        data = json.loads(res.data.decode('utf-8'))
+        assert 'Catalog for this auction does not exist. id: 997' in data['message']
 
-	def test_catalog_field_structure(self, client):
-		for auction in get_endpoint_result(client, '/api/auctions'):
-			catalog = get_endpoint_result(
-				client, '/api/catalog/{}'.format(auction['id'])
-			)
-			artwork_fields = {
-				'id': int,
-				'title': str,
-				'description': str,
-				'initial_price': str,
-				'sold_price': str,
-				'sold': bool,
-				'image_original': str,
-				'image_thumbnail': str,
-				'author': str,
-			}
+    def test_catalog_field_structure(self, client):
+        for auction in get_endpoint_result(client, '/api/auctions'):
+            catalog = get_endpoint_result(
+                client, '/api/catalog/{}'.format(auction['id'])
+            )
+            artwork_fields = {
+                'id': int,
+                'title': str,
+                'description': str,
+                'initial_price': str,
+                'sold_price': str,
+                'sold': bool,
+                'image_original': str,
+                'image_thumbnail': str,
+                'author': str,
+                'meta': dict,
+            }
 
-			for artwork in catalog:
-				for key in artwork_fields.keys():
-					assert type(artwork[key]) is artwork_fields[key]
+            for artwork in catalog:
+                for key in artwork_fields.keys():
+                    assert type(artwork[key]) is artwork_fields[key]
+                assert 'dimension' in artwork['meta']
+                assert isinstance(artwork['meta']['dimension'], list)
