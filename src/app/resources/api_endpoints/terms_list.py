0f152422b82b324fac1878880ba872b2ee1cb048
Adam Przywarty <przywarty.it@gmail.com>
2018-12-11 19:56:23 +0100

    Add artists searching.

--- src/app/resources/api_endpoints/terms_list.py
+++ src/app/resources/api_endpoints/terms_list.py
@@ -1,5 +1,6 @@
 from flask import request
 from flask_restful import Resource, abort
+from sqlalchemy import or_
 
 from app.models import models
 from app.api_utils import thumbnails, postmeta
@@ -8,11 +9,39 @@ from app.api_utils import thumbnails, postmeta
 class TermsList(Resource):
     def get(self):
         try:
-            page_number = int(request.args.get('page') or '0')
-            page_size = int(request.args.get('size') or '20')
-            return self._build_data_list(page_number, page_size)
+            search_query = request.args.get('search', None)
+            if search_query:
+                return self._handle_search(search_query)
+            else:
+                page_number = int(request.args.get('page') or '0')
+                page_size = int(request.args.get('size') or '20')
+                return self._build_data_list(page_number, page_size)
         except Exception as e:
             abort(404, message='Error querying Terms. {}'.format(e))
+
+    def _handle_search(self, search_query):
+        if len(search_query) < 3:
+            return []
+
+        filter_like = f'%{search_query}%'
+        authors = models.Terms.query.filter(
+            or_(
+                models.Terms.name.like(filter_like),
+                models.Terms.slug.like(filter_like)
+            )
+        ).order_by(
+            models.Terms.slug
+        ).all()
+
+        result = []
+        for author in authors:
+            taxonomy = models.TermTaxonomies.query.filter_by(
+                term_id=author.term_id,
+                taxonomy='autor'
+            ).first()
+            if taxonomy:
+                result.append(self._build_author(taxonomy))
+        return result
 
     def _build_data_list(self, page_number, page_size):
         result = []
--- src/app/tests/api_endpoints/test_terms_list.py
+++ src/app/tests/api_endpoints/test_terms_list.py
@@ -31,3 +31,32 @@ def test_404_if_invalid_page_or_size(client):
     res = client.get(f'/api/authors?page=pikpik&size=hahaha')
     data = json.loads(res.data.decode('utf-8'))
     assert 'Error querying Terms.' in data['message']
+
+
+def test_authors_search_query(client):
+    res = client.get(f'/api/authors?search=grajek')
+    data = json.loads(res.data.decode('utf-8'))
+    assert data == [
+        {
+            'id': 190,
+            'name': 'Daria Grajek',
+            'slug': 'daria-grajek',
+            'image_original': 'http://pragaleria.pl/wp-content/uploads/2017/03/1466354103.jpg',
+            'image_thumbnail': 'http://pragaleria.pl/wp-content/uploads/2017/03/1466354103-150x150.jpg',
+            'image_medium_thumbnail': 'http://pragaleria.pl/wp-content/uploads/2017/03/1466354103-300x220.jpg',
+            'image_big_thumbnail': 'http://pragaleria.pl/wp-content/uploads/2017/03/1466354103-737x540.jpg',
+            'image_medium': 'http://pragaleria.pl/wp-content/uploads/2017/03/1466354103-768x563.jpg',
+            'image_large': 'http://pragaleria.pl/wp-content/uploads/2017/03/1466354103-1024x750.jpg'
+        },
+        {
+            'id': 11,
+            'name': 'Dariusz Grajek',
+            'slug': 'dariusz-grajek',
+            'image_original': 'http://pragaleria.pl/wp-content/uploads/2017/03/1466351975.jpg',
+            'image_thumbnail': 'http://pragaleria.pl/wp-content/uploads/2017/03/1466351975-150x150.jpg',
+            'image_medium_thumbnail': 'http://pragaleria.pl/wp-content/uploads/2017/03/1466351975-300x200.jpg',
+            'image_big_thumbnail': 'http://pragaleria.pl/wp-content/uploads/2017/03/1466351975-800x533.jpg',
+            'image_medium': 'http://pragaleria.pl/wp-content/uploads/2017/03/1466351975-768x512.jpg',
+            'image_large': 'http://pragaleria.pl/wp-content/uploads/2017/03/1466351975-1024x683.jpg'
+        }
+    ]
