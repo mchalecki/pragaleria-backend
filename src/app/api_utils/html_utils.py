b5bf79ea03f4eade5e3af04c3b147ce0c3da8864
Marcin Chalecki <alcaster0@gmail.com>
2019-01-01 21:21:19 +0100

    Fix searching urls.

--- src/app/api_utils/html_utils.py
+++ src/app/api_utils/html_utils.py
@@ -1,6 +1,7 @@
 import re
 
 from bs4 import BeautifulSoup
+from flask import current_app
 
 URLS_TEMPLATES = {
     'virtual_tour': 'wnetrza3d.pl/realizacje/pragaleria/',
@@ -9,23 +10,25 @@ URLS_TEMPLATES = {
 
 
 def clean(source, urls=False):
-    if source:
-        soup = BeautifulSoup(source, "html.parser")
-        # current_app.logger.debug(soup)
-        text = soup.get_text(separator='\n')
-        text = '\n'.join(text.splitlines())
-        text = re.sub(r'\n+', '\n', text)
-        text = text.replace('\n \n', '\n')
-        text = text.strip()
-        text = text.replace('&quot;', '""')
-        if urls:
-            links = soup.find_all('a')
-            templates = URLS_TEMPLATES.copy()
-            existing_urls = {k: None for k in URLS_TEMPLATES}
-            for link in links:
-                for name, url_template in dict(templates).items():
-                    if url_template in link:
-                        existing_urls[name] = link
-                        templates.pop(name)
-            return text, existing_urls
+    if not source:
+        return None if not urls else (None, {k: None for k in URLS_TEMPLATES})
+    soup = BeautifulSoup(source, "html.parser")
+    text = soup.get_text(separator='\n')
+    text = '\n'.join(text.splitlines())
+    text = re.sub(r'\n+', '\n', text)
+    text = text.replace('\n \n', '\n')
+    text = text.strip()
+    text = text.replace('&quot;', '""')
+    if not urls:
         return text
+    else:
+        links = soup.find_all('a')
+        templates = URLS_TEMPLATES.copy()
+        existing_urls = {k: None for k in URLS_TEMPLATES}
+        for link in links:
+            link = link.get('href')
+            for name, url_template in dict(templates).items():
+                if url_template in link:
+                    existing_urls[name] = link
+                    templates.pop(name)
+        return text, existing_urls
