3629f06232469e0a447f713f5e893e55ee070734
rootie <rootie0011@gmail.com>
2019-01-16 16:21:44 -0500

    Refactor .travis.yml

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,14 @@
-cd pragaleria-backend
-docker-compose down
-git pull
-docker-compose up -d
+ #!/usr/bin/env sh
+
+SERVER_USER=$1
+SERVER_IP=$2
+SERVER_KEY=$3
+
+chmod 600 deploy
+ssh-add deploy
+ssh-keyscan -H $SERVER_IP >> ~/.ssh/known_hosts
+ssh -i $SERVER_KEY $SERVER_USER@$SERVER_IP "\
+    cd pragaleria-backend && \
+    docker-compose down && \
+    git pull && \
+    docker-compose up -d"
